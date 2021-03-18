dcb948bc1c195214ef76cf972c4c2c1640c89c1e
Jahid101 <jahidhasananik.official@gmail.com>
2021-03-19 02:21:36 +0600

    starting

---
+++ src/Components/Login/Firebase.Config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAc4uO9oXofEVo47Q2PQ8FIXjEltUCWAmY",
+    authDomain: "lets-ride-transport-ltd.firebaseapp.com",
+    projectId: "lets-ride-transport-ltd",
+    storageBucket: "lets-ride-transport-ltd.appspot.com",
+    messagingSenderId: "178064325521",
+    appId: "1:178064325521:web:3ef5baedb8d0df704c7830"
+  };
+
+  export default firebaseConfig;
+  
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+
+const Login = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+        </div>
+    );
+};
+
+export default Login;
